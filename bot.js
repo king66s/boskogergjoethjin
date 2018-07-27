01bc11f598eff3c596e4ba0091fb793ec8bff60b
king66s <40079399+king66s@users.noreply.github.com>
2018-07-27 20:24:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,44 +12,39 @@ KiNg66S.on('message', message => {
 
   
   if (message.content.startsWith(PREFIX + 'setwatch')) {
-            message.delete()
-
   KiNg66S.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
+    message.channel.sendMessage(`Watch Now: **${argresult}`)
 } 
 
  
   if (message.content.startsWith(PREFIX + 'setlis')) {
-               message.delete()
-
   KiNg66S.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
 } 
 
 
 if (message.content.startsWith(PREFIX + 'setname')) {
-             message.delete()
-
   KiNg66S.user.setUsername(argresult).then
+      message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
 } 
 
 if (message.content.startsWith(PREFIX + 'setavatar')) {
-             message.delete()
-
   KiNg66S.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
 if (message.content.startsWith(PREFIX + 'setstream')) {
-             message.delete()
-
   KiNg66S.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
      console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
 } 
 if (message.content.startsWith(PREFIX + 'setplay')) {
-             message.delete()
-
   KiNg66S.user.setGame(argresult);
      console.log('test' + argresult);
+    message.channel.sendMessage(`Playing: **${argresult}`)
 } 
 
 
