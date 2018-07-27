d18d7abf8ea6468dd0c4de6ed10e1d9e566bfc22
king66s <40079399+king66s@users.noreply.github.com>
2018-07-27 11:24:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,59 @@
 const Discord = require('discord.js');
-const PREFIX = "#"
-const fs = require('fs');
-var prefix = "#";
-const client = new Discord.Client();
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`in ${client.guilds.size} servers `)
-    console.log(`[Users] ${client.users.size}`)
+const PREFIX = "$"
+const KiNg66S = new Discord.Client();
+
+
+
+KiNg66S.on('message', message => {
+  if (!message.content.startsWith(PREFIX)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "441683368418082836") return;
+
+  
+  if (message.content.startsWith(PREFIX + 'setwatch')) {
+            message.delete()
+
+  KiNg66S.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('test' + argresult);
+} 
+
+ 
+  if (message.content.startsWith(PREFIX + 'setlis')) {
+               message.delete()
+
+  KiNg66S.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('test' + argresult);
+} 
+
+
+if (message.content.startsWith(PREFIX + 'setname')) {
+             message.delete()
+
+  KiNg66S.user.setUsername(argresult).then
+} 
+
+if (message.content.startsWith(PREFIX + 'setavatar')) {
+             message.delete()
+
+  KiNg66S.user.setAvatar(argresult);
+}
+
+if (message.content.startsWith(PREFIX + 'setstream')) {
+             message.delete()
+
+  KiNg66S.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
+     console.log('test' + argresult);
+} 
+if (message.content.startsWith(PREFIX + 'setplay')) {
+             message.delete()
+
+  KiNg66S.user.setGame(argresult);
+     console.log('test' + argresult);
+} 
+
+
 
 });
 
-client.on('ready', () => {
-    console.log('Login as ' + client.user.tag)
-})
-
-
- 
-client.on('guildMemberAdd', member => {
-    member.createDM().then(member => {
-        member.send(`:hearts:️:hearts:️ 
-حياكّ الله بيننا يمكن الصدف تخلق كثير 
-https://discord.gg/P6v7yvD
-**[ ${member} ] الدعووه خاصهه لسموك يا**`)
-    })
-})
-client.on('guildMemberRemove', member => {
-    member.createDM().then(member => {
-        member.send(`:hearts:️:hearts:️ 
-حياكّ الله بيننا يمكن الصدف تخلق كثير 
-https://discord.gg/P6v7yvD
-**[ ${member} ] الدعووه خاصهه لسموك يا**`)
-    })
-})
-
-client.login(process.env.BOT_TOKEN); 
+KiNg66S.login(process.env.BOT_TOKEN);
